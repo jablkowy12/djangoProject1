ad579ed33d0951b8664fa8e5f007cd7c714e6355
Ignacy Kujawa <253071@student.pwr.edu.pl>
2021-07-19 15:07:48 +0200

    changed requirements.txt and settings to work with heroku

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c(j7zqs_c_c6do#qptko=mmcr*r9l3a4$!8dlp-lq&x#7%#(u9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1' ]
 
 # Application definition
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
